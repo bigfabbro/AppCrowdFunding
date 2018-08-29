ab43cbbf430c6b888148fc6ae233048db95cb0e5
guerrinho96 <oscar962009@hotmail.it>
2018-08-29 11:41:46 +0200

    Bug fixed e aggiunta commenti

--- Smarty/templates/js/ricercaAv.js
+++ Smarty/templates/js/ricercaAv.js
@@ -1,16 +1,19 @@
+/**
+ * Funzione che si occupa di disabilitare le scelte non possibili nella ricerca avanzata; 
+ * Es: Se si selezione 'Utente' non si può ricercare per categoria. 
+ */
 $(document).ready(function() {
 	//inputKey1 è l'id del primo select
     $("#inputKey1").change(function() {
-		//option:nth(numero), numero è l'indice dell'elemento che ti annulla le cose da selezionare nell'altro select
         if ($("#inputKey1 option:nth(1)").is(":selected")){
-		//inputKey2 è il secondo select; option:nth(2) è quello da disabilitare
+		//inputKey2 è il secondo select; 
             $("#inputKey2 option:nth(1)").attr('disabled', '');
             $("#inputKey2 option:nth(0)").attr('disabled', '');
 			      $("#inputKey2").val($("#inputKey2 option:nth(2)" ).val())
 		}
-		else
-			//qui lo riabiliti
+		else{
 			$("#inputKey2 option:nth(0)").removeAttr('disabled');
-      $("#inputKey2 option:nth(1)").removeAttr('disabled');
+			$("#inputKey2 option:nth(1)").removeAttr('disabled');
+		}
     });
 });
